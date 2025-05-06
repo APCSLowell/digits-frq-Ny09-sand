fb9512e00c5058fcc1d48c50fbdd0ae830fd15bd
Ny09-sand <nicholas.yangwt@gmail.com>
2025-05-05 18:34:49 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,24 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    if ( num == 0 ) {digitList.add(num); } 
+		else{ while(num!=0){
+			digitlist.add(0,num%10);
+			num=num/10; 
+		} 
+			
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		boolean sure = false; 
+		for ( int i = 1; i< digitList.size(); i+=) {
+			if( digitList.get(i)<digitList.get(i-1){
+				sure = false; 
+			} 
+		} 
+		return sure; 
 	}
 	
 	public String toString()
